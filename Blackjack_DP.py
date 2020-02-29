b3aba882edbfaa36dfe8226e37074fc15de10a38
sailboat642 <sailboat642@gmail.com>
2020-02-29 21:24:51 +0530

    updates python files

--- Blackjack_DP.py
+++ Blackjack_DP.py
@@ -1,7 +1,6 @@
 import numpy as np
 import random
-
-V = np.array([[[0.]*10]*2]*10)
+import cProfile
 
 states = []
 for i in range(10):
@@ -10,37 +9,55 @@ for i in range(10):
             states.append((i, j, k))
 
 dealer_dist = [
-    [13.6,  5.6, 14.3, 14.6, 13.8, 38.1],
-    [34.9, 13.6, 14.1, 12.8, 12.8, 11.9],
-    [38.1, 11.8, 13.4, 12.6, 12.2, 12.0],
-    [39.6, 11.9, 13.3, 12.1, 11.8, 11.4],
-    [42.1, 12.1, 12.4, 11.0, 11.6, 10.9],
-    [43.5, 12.3, 11.8, 11.0, 11.2, 10.3],
-    [26.3, 37.0, 14.1,  7.9,  7.8,  7.0],
-    [25.0, 12.7, 35.6, 12.8,  7.3,  6.6],
-    [22.3, 12.0, 12.0, 35.6, 11.9,  6.2],
-    [21.3, 11.3, 11.5, 11.1, 34.1, 11.3]
+    [13.6,  5.6 , 14.3, 14.6, 13.8, 38.1],
+    [34.9, 13.6 , 14.1, 12.8, 12.8, 11.9],
+    [38.1, 11.8 , 13.4, 12.6, 12.2, 12.0],
+    [39.6, 11.9 , 13.3, 12.1, 11.8, 11.4],
+    [42.1, 12.1 , 12.4, 11.0, 11.6, 10.9],
+    [43.5, 12.3 , 11.8, 11.0, 11.2, 10.3],
+    [26.3, 37.0 , 14.1,  7.9,  7.8,  7.0],
+    [25.0, 12.7 , 35.6, 12.8,  7.3,  6.6],
+    [22.3, 12.0 , 12.0, 35.6, 11.9,  6.2],
+    [21.3, 11.25, 11.5, 11.1, 34.1, 11.3]
 ]
 
 # cumulative win probability
 # sum of all indexes before
 for probabilities in dealer_dist:
     for i in range(1, 6):
-        probabilities[i] += probabilities[i-1]
+        probabilities[i] += probabilities[i-1]    
+
+pi = {}
+for state in states:
+    pi[state] = {
+        0 : 0,
+        1 : 1
+    }
+
+
+# random policy
+for h in range(10):
+    for a in range(2):
+        for d in range(10):
+            r = np.random.random()
+            pi[(h, a, d)][1] = r
+            pi[(h, a, d)][0] = 1 - r
+
+
 
 def blackjack_probability(s_, s, a, r):
     stay = 0
     hit = 1
 
-    if not (r == 0 or r == 1):
-        return 0
-    
     if a == stay:
         '''find Probability of win, loss, tie'''
         if s[0] < 5:
             # probability dealer busts
             if r == 1:
                 return (dealer_dist[s[2]][0])/100
+            
+            elif r == 0:
+                return 0
 
             else:
                 return (1 - dealer_dist[s[2]][0]/100)
@@ -48,27 +65,37 @@ def blackjack_probability(s_, s, a, r):
         else:
             index = s[0] - 4      # 17 is 1
             if r == 1:
+                # probability of dealer recieving a lower score
                 return (dealer_dist[s[2]][index-1]/ 100)
             
             elif r == 0:
-                return ((dealer_dist[s[2]][index] - dealer_dist[s[2]-1][index-1])/100)
+                # probability of dealer getting player's score
+                return ((dealer_dist[s[2]][index] - dealer_dist[s[2]][index-1])/100)
             
             else:
+                # probability of dealer getting a higher score
                 return (1-(dealer_dist[s[2]][index]/100))
                 
     
     if a == hit:
         '''Find probability s_'''
-        if s_[2] != s_[2]:
-            '''dealer card is not the same'''
-            return 0
         
         if s_ == (-1, -1, -1):
             '''find Probability of a bust'''
-            return (4 + s[0])/13
+            if s[1] == 0:
+                return (4 + s[0])/13
+            else:
+                # player cannot bust with an ace
+                return 0
+            
+        if s[2] != s_[2]:
+            '''dealer card is not the same'''
+            return 0
+        
             
         if s_[1] == 0 and s[1] == 1:
             '''player has to use his ace'''
+            
             if s_[0] == s[0]:
                 '''probability of a 10 value card'''
                 return 4/13
@@ -84,59 +111,57 @@ def blackjack_probability(s_, s, a, r):
                 '''new hand cannot be less than old hand'''
                 return 0
         
-        return 1/13
+            return 1/13
+        
+        return 0
 
 
-def pi_20(action, state):
-    '''deterministic policy that returns the probability of action given state'''
-    stay = 0
-    hit = 1
-    
-    if state[0] >= 8:
-        if action == stay:  
-            return 1
-        else:
-            return 0
-    else:
-        if action == hit:
-            return 0
-        else:
-            return 1
-
-@profile
-def evaluate_policy(V, theta):
+def evaluate_policy(V, theta, pi):
     while True:
         delta = 0
         for s1, s2, s3 in states:
             v = V[s1][s2][s3]
-            bellman_update(V, (s1, s2, s3))
+            if abs(v) > 2:
+                return
+            bellman_update(V, (s1, s2, s3), pi)
             delta = max(delta, abs(v - V[s1][s2][s3]))
-
         if delta < theta:
             break
     return V
 
-@profile
-def bellman_update(V, state):
+
+def bellman_update(V, state, pi):
     """Mutate ``V`` according to the Bellman update equation."""
     stay = 0
     hit = 1
     
     hit_value = 0
+    probability = [0, 0]
     for s_ in states:
         Vs_ = V[s_[0]][s_[1]][s_[2]]
-        hit_value += blackjack_probability(s_, state, hit, 0)*(0 + Vs_) 
+        hit_value += blackjack_probability(s_, state, hit, 0)*(0 + Vs_)
+        probability[0] += blackjack_probability(s_, state, hit, 0)
+        
     
     # probability of a bust
-    hit_value += blackjack_probability((-1, -1, -1), state, hit, 0)*(-1 + 0) 
+    hit_value += blackjack_probability((-1, -1, -1), state, hit, -1)*(-1 + 0)
+    probability[0] += blackjack_probability((-1, -1, -1), state, hit, -1)
     
     # value of staying
     stay_value = 0
-    for r in range(3):
-        stay_value += blackjack_probability((-1, -1, -1), state, stay, (r-1))*(r-1)
+    for r in [-1, 0, 1]:
+        stay_value += blackjack_probability((-1, -1, -1), state, stay, r)*(r + 0)
+        probability[1] += blackjack_probability((-1, -1, -1), state, stay, r)
+       
     
     hand, ace, dealer = state
-    V[hand][ace][dealer] = pi_20(1, state)*hit_value + pi_20(0, state)*stay_value
+    V[hand][ace][dealer] = pi[state][hit]*hit_value + pi[state][stay]*stay_value
     return 
 
-evaluate_policy(V, 0.002)
+
+def error(v):
+    return (abs(1 - v))
+
+
+V = np.zeros((10, 2, 10))
+cProfile.run("evaluate_policy(V, 0.000001, pi)")
--- Blackjack_Monte-Carlo.py
+++ Blackjack_Monte-Carlo.py
@@ -1,39 +1,49 @@
 import numpy as np
 import random
 
-V = np.array([[[0.]*10]*2]*10)
-visit_count = np.array([[[0.]*10]*2]*10)
+states = []
+for i in range(10):
+    for j in range(2):
+        for k in range(10):
+            states.append((i, j, k))
 
-def pi_20(hand, dealer_card):
-    player_sum = sum(hand)
-    
-    if 1 in hand and player_sum < 12:
-            player_sum += 10
-    
-    if player_sum < 20:
-        return 'hit'
-    else:
-        return 'stick'
+pi = {}
+for state in states:
+    pi[state] = {
+        0 : 0,
+        1 : 1
+    }
 
+# random policy
+for h in range(10):
+    for a in range(2):
+        for d in range(10):
+            r = np.random.random()
+            pi[(h, a, d)][1] = r
+            pi[(h, a, d)][0] = 1 - r
+
+
+V = np.zeros((10, 2, 10))
+visit_count = np.zeros((10, 2, 10))
 
 def update_values(states, r, V, visit_count):
     for h, a, d in states:
         V[h][a][d] = V[h][a][d] + (1/visit_count[h][a][d])*(r - V[h][a][d]) 
 
 
-array =([(3.1, 2), (2.1, 4)])
-@profile
 def blackjack_hand_result(V, visit_count):
     #Deck of cards, 4 types of 10
+    
     deck=np.array([1,2,3,4,5,6,7,8,9,10,10,10,10])
     
     #Creating a random hand if none is input
+    
     player_hand=[random.choice(deck), random.choice(deck)]
         
     dealer_card=random.choice(deck)
     
     # keeps track of all states that the hand has been in during one play
-    states = []
+    states_visited = []
     
     player_sum = sum(player_hand)
     
@@ -43,9 +53,10 @@ def blackjack_hand_result(V, visit_count):
         usable_ace = 1
         player_sum += 10
     
-    
+    # update states
     if player_sum > 11:
-        states.append((player_sum-12, usable_ace, dealer_card-1))
+        state = (player_sum-12, usable_ace, dealer_card-1)
+        states_visited.append(state)
         visit_count[player_sum-12][usable_ace][dealer_card-1] += 1
         
     # keeps track of dealers hand
@@ -59,38 +70,39 @@ def blackjack_hand_result(V, visit_count):
 
         dealer_cards.append(random.choice(deck))
         if 1 in (dealer_cards) and sum(dealer_cards)==11:
-            update_values(states, 0, V, visit_count)
+            update_values(states_visited, 0, V, visit_count)
             return 0
         
-        update_values(states, 1, V, visit_count)
+        update_values(states_visited, 1, V, visit_count)
         return 1
     
 
     else:
     # Seeing how often it says to 'hit'
+        hit = 1
+        stay = 0
 
-        while pi_20(player_hand, dealer_card) == 'Hit':
+        while player_sum < 12 or np.random.random() < pi[state][hit]:
             #Adding one card for every hit
             new_card = random.choice(deck)
             player_hand.append(new_card)
+            player_sum = sum(player_hand)
             
-            player_sum += new_card
+            usable_ace = 0
             
-            if new_card == 1 and player_sum < 12:
+            if 1 in player_hand and player_sum < 12:
                 usable_ace = 1
                 player_sum += 10
             
-            if usable_ace == 1 and player_sum > 21:
-                player_sum -= 10
-                usable_ace == 0
-
+            state = (player_sum-12, usable_ace, dealer_card-1)
+            
             #Player loses bet if hand goes above 21
-            if player_sum>21:
-                update_values(states, -1, V, visit_count)
+            if player_sum > 21:
+                update_values(states_visited, -1, V, visit_count)
                 return -1
             
             if player_sum > 11:
-                states.append((player_sum-12, usable_ace, dealer_card-1))
+                states_visited.append(state)
                 visit_count[player_sum-12][usable_ace][dealer_card-1] += 1
             
     while True:
@@ -111,6 +123,7 @@ def blackjack_hand_result(V, visit_count):
 
         #If dealer gets blackjack you lose even if you have 21
         if len(dealer_cards)==2 and soft_score==21:
+            update_values(states_visited, -1, V, visit_count)
             return -1
         
         #Dealer stays on all 17s
@@ -118,22 +131,22 @@ def blackjack_hand_result(V, visit_count):
 
             #If dealer bust, player wins bet
             if soft_score>21:
-                update_values(states, 1, V, visit_count)
+                update_values(states_visited, 1, V, visit_count)
                 return 1
 
             #If player has more than dealer, player wins bet
             if player_sum>soft_score:
-                update_values(states, 1, V, visit_count)
+                update_values(states_visited, 1, V, visit_count)
                 return 1
 
             #Tie means no money changes hands
             if player_sum==soft_score:
-                update_values(states, 0, V, visit_count)
+                update_values(states_visited, 0, V, visit_count)
                 return 0
 
             #If player has lower, player loses bet
             if player_sum<soft_score:
-                update_values(states, -1, V, visit_count)
+                update_values(states_visited, -1, V, visit_count)
                 return -1
 
 
@@ -142,3 +155,4 @@ def blackjack_sim(n_hands, V, visit_count):
         blackjack_hand_result(V, visit_count)
      
 blackjack_sim(50_000, V, visit_count)
+print(V)
